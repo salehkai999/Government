d8b7929a0577c3c84251589222c3e171d59a65d0
saleh <saleh.mk96@gmail.com>
2020-11-08 22:15:38 -0600

    Fully working just needs some modifications.

--- app/src/main/java/com/saleh/government/OfficialActivity.java
+++ app/src/main/java/com/saleh/government/OfficialActivity.java
@@ -6,11 +6,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
@@ -66,17 +69,32 @@ public class OfficialActivity extends AppCompatActivity {
                     case "Democratic Party" :
                         partyImage.setImageResource(R.drawable.dem_logo);
                         findViewById(R.id.offLayout).setBackgroundColor(getResources().getColor(R.color.blue));
+                        partyImage.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                openDemWebSite();
+                            }
+                        });
                         break;
                     case "Republican Party":
                         partyImage.setImageResource(R.drawable.rep_logo);
                         findViewById(R.id.offLayout).setBackgroundColor(getResources().getColor(R.color.red));
+                        partyImage.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                openRepWebSite();
+                            }
+                        });
                         break;
                     default:
                         partyImage.setVisibility(View.INVISIBLE);
                         findViewById(R.id.offLayout).setBackgroundColor(getResources().getColor(R.color.black));
                 }
-                if(off.getAddress() != null)
+                if(off.getAddress() != null) {
                     addressData.setText(off.getAddress());
+                    Linkify.addLinks(addressData,Linkify.ALL);
+                }
+
                 else {
                     addressView.setVisibility(View.INVISIBLE);
                     addressData.setVisibility(View.INVISIBLE);
@@ -87,6 +105,7 @@ public class OfficialActivity extends AppCompatActivity {
                     for(int i=0;i<off.getPhones().size();i++)
                         phone+=off.getPhones().get(i).toString()+"\n";
                     phoneData.setText(phone);
+                    Linkify.addLinks(phoneData,Linkify.ALL);
                 }
                 else{
                     phoneData.setVisibility(View.INVISIBLE);
@@ -98,6 +117,7 @@ public class OfficialActivity extends AppCompatActivity {
                     for(int i=0;i<off.getUrls().size();i++)
                         websites+=off.getUrls().get(i).toString()+"\n";
                     websiteData.setText(websites);
+                    Linkify.addLinks(websiteData,Linkify.ALL);
                 }
                 else {
                     websiteView.setVisibility(View.INVISIBLE);
@@ -105,17 +125,125 @@ public class OfficialActivity extends AppCompatActivity {
                 }
 
                 downloadImage();
+                socialMedia();
             }
         }
 
 
     }
 
+    private void openDemWebSite() {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.democrats)));
+        startActivity(i);
+    }
+
+    private void openRepWebSite() {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.republican)));
+        startActivity(i);
+    }
+
+    private void socialMedia(){
+        Log.d(TAG, "socialMedia: "+off.getChannels().toString());
+        if(off.getChannels().get("Facebook") != null){
+            Log.d(TAG, "socialMedia: "+off.getChannels().toString());
+            fbImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(OfficialActivity.this, "FACEBOOK "+off.getChannels().get("Facebook"), Toast.LENGTH_SHORT).show();
+                    openFBPage(off.getChannels().get("Facebook"));
+                }
+            });
+        }
+        else {
+            fbImg.setVisibility(View.INVISIBLE);
+        }
+
+        if(off.getChannels().get("Twitter") != null){
+            Log.d(TAG, "socialMedia: "+off.getChannels().toString());
+            twtImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(OfficialActivity.this, "TWT "+off.getChannels().get("Twitter"), Toast.LENGTH_SHORT).show();
+                    openTwtPage(off.getChannels().get("Twitter"));
+                }
+            });
+        }
+        else {
+            twtImg.setVisibility(View.INVISIBLE);
+        }
+
+        if(off.getChannels().get("YouTube") != null){
+            Log.d(TAG, "socialMedia: "+off.getChannels().toString());
+            ytImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(OfficialActivity.this, "YT "+off.getChannels().get("YouTube"), Toast.LENGTH_SHORT).show();
+                    openYTPage(off.getChannels().get("YouTube"));
+                }
+            });
+        }
+        else {
+            ytImg.setVisibility(View.INVISIBLE);
+        }
+
+
+
+    }
+
+    private void openFBPage(String id){
+        String FACEBOOK_URL = "https://www.facebook.com/" + id;
+
+        Intent intent;
+        String urlToUse;
+        try {
+            getPackageManager().getPackageInfo("com.facebook.katana", 0);
+
+            int versionCode = getPackageManager().getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                urlToUse = "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                urlToUse = "fb://page/" + id;
+            }
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlToUse));
+        } catch (Exception e) {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(FACEBOOK_URL));
+        }
+
+        startActivity(intent);
+    }
+
+    private void openTwtPage(String id){
+        String twitterAppUrl = "twitter://user?screen_name=" + id;
+        String twitterWebUrl = "https://twitter.com/" + id;
+
+        Intent intent;
+        try {
+            getPackageManager().getPackageInfo("com.twitter.android", 0);
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(twitterAppUrl));
+        } catch (Exception e) {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(twitterWebUrl));
+        }
+        startActivity(intent);
+    }
+
+    private void openYTPage(String id){
+        Intent intent = null;
+        try {
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setPackage("com.google.android.youtube");
+            intent.setData(Uri.parse("https://www.youtube.com/"+id));
+            startActivity(intent);
+        }
+        catch (Exception e){
+            intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://wwww.youtube.com/"+id));
+            startActivity(intent);
+        }
+    }
 
     private void downloadImage(){
         if(!off.getPhotoUrl().equals("none")) {
         Log.d(TAG, "downloadImage: "+off.getPhotoUrl());
-        Picasso.get().load(off.getPhotoUrl()).error(R.drawable.brokenimage)
+        Picasso.get().load(off.getPhotoUrl().toString()).error(R.drawable.brokenimage)
                 .placeholder(R.drawable.placeholder).into(offImage, new Callback() {
             @Override
             public void onSuccess() {
@@ -127,10 +255,23 @@ public class OfficialActivity extends AppCompatActivity {
                 Log.d(TAG, "onError: "+e);
             }
         });
+        offImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPictureActivity();
+            }
+        });
+
         }
         else {
             offImage.setImageResource(R.drawable.missing);
             Log.d(TAG, "downloadImage: "+"NO IMAGE");
         }
+    }
+
+    private void openPictureActivity() {
+        Intent intent = new Intent(this,PictureActivity.class);
+        intent.putExtra("off",off);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/saleh/government/PictureActivity.java
@@ -0,0 +1,99 @@
+package com.saleh.government;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+public class PictureActivity extends AppCompatActivity {
+
+    private TextView posText;
+    private TextView nameText;
+    private ImageView offImg;
+    private ImageView partyImg;
+    private Officals off;
+    private static final String TAG = "PictureActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+        posText = findViewById(R.id.picPosText);
+        nameText = findViewById(R.id.picNameText);
+        offImg = findViewById(R.id.picOffImg);
+        partyImg = findViewById(R.id.picParty);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("off")){
+            off = (Officals) intent.getSerializableExtra("off");
+            posText.setText(off.getPosition());
+            nameText.setText(off.getName());
+            setPartyLogo();
+            downloadImage();
+        }
+    }
+
+    private void setPartyLogo() {
+        switch (off.getParty()){
+            case "Democratic Party" :
+                partyImg.setImageResource(R.drawable.dem_logo);
+                findViewById(R.id.picLayout).setBackgroundColor(getResources().getColor(R.color.blue));
+                partyImg.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openDemWebSite();
+                    }
+                });
+                break;
+            case "Republican Party":
+                partyImg.setImageResource(R.drawable.rep_logo);
+                findViewById(R.id.picLayout).setBackgroundColor(getResources().getColor(R.color.red));
+                partyImg.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openRepWebSite();
+                    }
+                });
+                break;
+            default:
+                partyImg.setVisibility(View.INVISIBLE);
+                findViewById(R.id.picLayout).setBackgroundColor(getResources().getColor(R.color.black));
+        }
+    }
+
+    private void openDemWebSite() {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.democrats)));
+        startActivity(i);
+    }
+
+    private void openRepWebSite() {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.republican)));
+        startActivity(i);
+    }
+
+    private void downloadImage(){
+        if(!off.getPhotoUrl().equals("none")) {
+            Log.d(TAG, "downloadImage: "+off.getPhotoUrl());
+            Picasso.get().load(off.getPhotoUrl().toString()).error(R.drawable.brokenimage)
+                    .placeholder(R.drawable.placeholder).into(offImg, new Callback() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.d(TAG, "onError: "+e);
+                }
+            });
+
+        }
+    }
+}
